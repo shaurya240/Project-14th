a6ceda40f1b79a2fddb60f16605d2157b79e80d3
Shaurya <shaurya240@gmail.com>
2026-02-01 21:31:14 -0700

    Update

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dh80wqolw/video/upload/v1770006421/Ed-Sheeran-Sapphire-_VoxSongs.com_d8aktn.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dh80wqolw/video/upload/v1770006592/ed-sheeran-sapphire-voxsongscom_rqOfre8Z_iaf2pn.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
