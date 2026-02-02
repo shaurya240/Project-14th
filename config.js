ba573d3c077c2a451f8a68bb518e4e604560483b
Shaurya <shaurya240@gmail.com>
2026-02-01 21:20:50 -0700

    Update

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yayyyyy! am i not the luckiest??! 🎉💝💖💝💓",
+        title: "No going back now 🎉💝💖💝💓",
         message: "See u on discord on 14th, 10000000 hugs and kisses vvvv soon!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
